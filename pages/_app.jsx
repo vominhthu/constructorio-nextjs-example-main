53b64b983d593ded57ccc7d2408dc7967a7395cf
VO MINH THU <thu.vominh23@gmail.com>
2023-08-23 14:47:07 +0700

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
     <Head>
       <title>Constructor Next.js Example</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="google-site-verification" content="Qw9E4neXTQd51bXSJ5ImjVnYAIPtdMTw48VdGfry9xE" />
     </Head>
     <Component {...pageProps} />
   </div>
